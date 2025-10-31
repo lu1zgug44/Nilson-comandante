c7e7c86dfee60c66d7ee82740ee6d7b8ab3a7ec2
LUIZ GUSTAVO CARVALHO DA SILVA COSTA <0000111775618xsp@al.educacao.sp.gov.br>
2025-10-31 18:05:39 -0300

    parte 4

--- Comanda.sql
+++ Comanda.sql
@@ -1,3 +1,26 @@
 drop database if exists Fast_Food;
 create database Fast_Food;
-use Fast_Food
+use Fast_Food;
+
+-- Cliente -- Funcionário -- Comida
+-- Bebidas -- Cardápio -- SAC -- Mesa
+-- Comanda/Pedido
+
+create table atendente(
+
+);
+
+create table ItensPedido(
+
+);
+
+create table Mesa(
+
+);
+
+create table Produto(
+CodProd int primary key auto_increment,
+descricao varchar(50) not null,
+QtaProduto int,
+ValorUnitario decimal(10,2)
+);
