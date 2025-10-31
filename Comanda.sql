ef11a1cf4573cc1c4e4396a7888f51aa34c79eb0
LUIZ GUSTAVO CARVALHO DA SILVA COSTA <0000111775618xsp@al.educacao.sp.gov.br>
2025-10-31 18:10:36 -0300

    parte 5

--- Comanda.sql
+++ Comanda.sql
@@ -7,7 +7,15 @@ use Fast_Food;
 -- Comanda/Pedido
 
 create table atendente(
-
+CodAtendente int primary key auto_increment,
+NomeAtendente varchar(50) not null, 
+logradouro varchar(50) not null, 
+Bairro varchar(50) not null, 
+CEP int not null, 
+Municipio varchar(70) not null, 
+CPF bigint not null, 
+RG bigint not null, 
+Telefone bigint not null, 
 );
 
 create table ItensPedido(
